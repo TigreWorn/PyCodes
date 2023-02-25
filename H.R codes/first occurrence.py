6b3f8b327b914998c8878da0fc4592fac998ca2f
TigreWorn <122634216+TigreWorn@users.noreply.github.com>
2023-02-25 16:35:32 +0530

    Create  first occurrence.py

---
+++ H.R codes/first occurrence.py
@@ -0,0 +1,8 @@
+import re
+s = re.findall(f"[A-Za-z0-9]",input())
+if s:
+    print(s[0])
+else:
+    print(-1)
+#You are given a string .
+#Your task is to find the first occurrence of an alphanumeric character in  (read from left to right) that has consecutive repetitions.
